5635600f74e30d23bd9b0d63120611caa111f47d
Imran Syam <md.imransyam1956@gmail.com>
2025-03-01 22:13:54 +0600

    Implemented role based access comtrol by using Spatie Laravel Permission package

---
+++ database/seeders/RoleSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Role::create(['name' => 'admin']);
+        Role::create(['name' => 'user']);
+
+        $user = User::find(1); // Change ID as needed
+        $user->assignRole('admin');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -26,6 +26,11 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    // Route::get('/admin/dashboard', [AdminController::class, 'index'])->name('admin.dashboard')->middleware('role:Admin');
+
 });
+
+
 
 require __DIR__.'/auth.php';
